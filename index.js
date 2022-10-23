85c201b2d7e0fe6890c0cd2c2c6adff0622066d2
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-10-23 08:45:40 +0600

    ok

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+
+app.get('/',(req,res) =>{
+    res.send('News API...');
+});
+app.get('/news-categories',(req,res) =>{
+    res.send(categories);
+});
+app.get('/category/:id',(req,res) =>{
+    const id = req.params.id;
+    if(id === '08')
+    {
+        res.send(news);
+    }
+    else{
+    const categoryNews = news.filter( n => n.category_id === id);
+    res.send(categoryNews);
+    }
+});
+app.get('/news',(req,res) =>{
+    res.send(news);
+});
+app.get('/news/:id',(req,res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find( n => n._id === id);
+    res.send(selectedNews);
+});
+
+app.listen(port,() =>{
+    console.log("Server Running");
+})
